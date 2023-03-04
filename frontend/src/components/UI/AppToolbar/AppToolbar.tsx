a5701e131ceb9294c164a84091ac8db64e185aa8
Emir Marat <emmarrat@gmail.com>
2023-03-04 17:52:38 +0600

    update: updated UI of the Appbar

--- frontend/src/components/UI/AppToolbar/AppToolbar.tsx
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from '../../../app/hooks';
 import { selectUser } from '../../../features/users/usersSlice';
 import UserMenu from './UserMenu';
 import AnonymousMenu from './AnonymousMenu';
+import LocalMallRoundedIcon from '@mui/icons-material/LocalMallRounded';
 
 const Link = styled(NavLink)({
   color: 'inherit',
@@ -22,7 +23,16 @@ const AppToolbar = () => {
       <Toolbar>
         <Grid container justifyContent="space-between" alignItems="center">
           <Typography variant="h6" component="div">
-            <Link to="/" sx={{display: 'flex', alignItems: 'center'}}>Exam 11
+            <Link to="/" sx={{display: 'flex', alignItems: 'center'}}>JS-17's market place
+              <div style={{
+                borderRadius: '50%',
+                width: '32px', height: '32px',
+                backgroundColor: '#7ba5f1',
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                marginLeft: '10px'
+              }}>
+                <LocalMallRoundedIcon fontSize="medium"/>
+              </div>
             </Link>
           </Typography>
           <Grid item>
